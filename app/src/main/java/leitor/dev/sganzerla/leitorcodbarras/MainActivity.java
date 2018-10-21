34d6ee5a20908de1d8a9164e3a78c76063a9429e
antoni.sganzerla <antoni.sganzerla@egsys.com.br>
2018-10-20 21:35:21 -0300

    config inicial projeto

---
+++ app/src/main/java/leitor/dev/sganzerla/leitorcodbarras/MainActivity.java
@@ -0,0 +1,13 @@
+package leitor.dev.sganzerla.leitorcodbarras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
