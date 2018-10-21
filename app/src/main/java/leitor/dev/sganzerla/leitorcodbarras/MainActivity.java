ed70d15a1715e9cadcf7c8e70343fa06b4177ddf
antoni.sganzerla <antoni.sganzerla@egsys.com.br>
2018-10-20 22:47:44 -0300

    versão demo

--- app/src/main/java/leitor/dev/sganzerla/leitorcodbarras/MainActivity.java
+++ app/src/main/java/leitor/dev/sganzerla/leitorcodbarras/MainActivity.java
@@ -1,13 +1,74 @@
 package leitor.dev.sganzerla.leitorcodbarras;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity{
+
+    private Button buttonRead;
+    private Button buttonCopy;
+    private EditText editTextBarCode;
+    public static final int COD = 2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonRead = findViewById(R.id.button_ler);
+        buttonCopy = findViewById(R.id.button_copiar);
+        editTextBarCode = findViewById(R.id.edittext_codigo_barras);
+
+        buttonRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,SimpleScannerActivity.class);
+                startActivityForResult(intent,COD);
+            }
+        });
+
+        buttonCopy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //ClipboardManager Copiar = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                Toast.makeText(MainActivity.this,"Copiado para Área de Transferência", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == COD){
+            if(data != null){
+                Bundle extras = data.getExtras();
+                if(extras != null){
+                    String codigo = extras.getString("codigo");
+                    editTextBarCode.setText(codigo);
+                }else{
+                    showError();
+                }
+            }else{
+                showError();
+            }
+        }
+    }
+
+    private void showError(){
+        Toast.makeText(MainActivity.this,"Não foi possível ler o código.", Toast.LENGTH_SHORT).show();
+        editTextBarCode.getText().clear();
+    }
+
 }
---
+++ app/src/main/java/leitor/dev/sganzerla/leitorcodbarras/SimpleScannerActivity.java
@@ -0,0 +1,53 @@
+package leitor.dev.sganzerla.leitorcodbarras;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class SimpleScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+
+    private ZXingScannerView mScannerView;
+    private static final String TAG = "BARCODE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_scanner);
+
+        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
+        setContentView(mScannerView);
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        Log.v(TAG, rawResult.getText()); // Prints scan results
+        Log.v(TAG, rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+
+        // If you would like to resume scanning, call this method below:
+        mScannerView.resumeCameraPreview(this);
+
+        Intent intent = new Intent();
+        intent.putExtra("codigo", rawResult.getText());
+        setResult(MainActivity.COD, intent);
+        finish();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+}
